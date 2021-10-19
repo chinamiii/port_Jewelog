8a6f104f9e6ee19442f966f899792c012cd75524
chinamiii <chinami.2929@icloud.com>
2021-10-19 12:34:50 +0000

    [relation css]

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,11 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :items, dependent: :destroy
 
-  def favorited_by?(user)
-    favorites.where(user_id: user.id).exists?
-  end
+
+   def favorited_by?(user)
+     favorites.where(user_id: user.id).exists?
+   end
+
 
   # attachment :post_image
   has_many_attached :post_images
