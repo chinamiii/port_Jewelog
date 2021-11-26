2a697b221b3de8748e436d64015976ad0796250b
chinamiii <chinami.2929@icloud.com>
2021-11-26 07:43:30 +0000

    [edit ja.yml]

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module Jewelog
     config.i18n.default_locale = :ja # デフォルトのlocaleを日本語(:ja)にする
     config.paths.add 'lib', eager_load: true # 追加
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
